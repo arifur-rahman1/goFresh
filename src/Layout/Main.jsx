504c8b7d0e62ccd7ad4bb642d74318b4f0655fe7
arifur-rahman1 <aarifurrahman93@gmail.com>
2024-01-19 10:52:02 +0600

    footer done

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 const Main = () => {
     return (
         <>
+            <div className="min-h-screen">
             <Navbar></Navbar> 
             <Outlet></Outlet>
+            <div className="sticky top-[100vh]">
+            <Footer></Footer>
+            </div>
+            </div>
         </>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'title':'hsl(var(--title))',
+        'secondary-title':'hsl(var(--secondary-title))',
+        'primary-bg':'hsl(var(--primary-bg))',
+        'secondary-bg':'hsl(var(--secondary-bg))'
+      },
+    },
   },
   plugins: [require("daisyui")],
+  daisyui:{
+    themes:[ {
+      light: {
+        ...require("daisyui/src/theming/themes")["[data-theme=light]"],
+        '--title': '126, 16.8%, 37.3%',
+        '--primary-bg': '98, 33%, 84%',
+        '--secondary-bg': '125, 16%, 59%',
+        '--secondary-title': '118, 12%, 51%',
+      },
+    },]
+      
+  }
 }
 
